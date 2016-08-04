fdc5e8d0c244b0f9b1750f406688109bf847baa3
back <591580817@qq.com>
2016-08-04 11:40:40 +0800

    20160804

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Red Flag
+# <center> [**RED FLAG**](http://www.红旗学院.com)
+## 1. <font size=6>首页介绍
+* <font size=4 color=red>**网络空间**</font><font size=3> —— 国家的第五疆城</font>
+![red flag](images/bg_01.png)
+
+## 2. <font size=6>内院弟子
+* <font size=4 color=red>**红旗学院**</font><font size=3> 实景学习氛围</font>
+	
+	![red flag](images/zd_01.png)![red flag](images/zd_02.png)
+* <font size=4 color=red>**红旗学院**</font><font size=3> 行业、企业分享</font>
+
+	![red flag](images/zd_03.png) ![red flag](images/zd_04.png)
+* <font size=4 color=red>**红旗学院**</font><font size=3> 学长分享</font>
+
+	![red flag](images/zd_05.png) ![red flag](images/zd_06.png)
+## 3. <font size=6>外院弟子
+<font size=5 color=red>红旗学院 首创网络安全人才私人职业服务体系：</font>
+<font size=3>响应习总书记关于网络安全人才特殊待遇的指示，以红旗学院为中心，创建专业的人才输送平台，打造“私人定制”级的就业服务体系，实现用人单位和学员的广泛接触和无缝对接，帮助红旗学院的学员实现</font><font size=4 color=red>三 年 收 入 破 30 万 ！</font>
+
+
+## 4. <font size=6>红旗战队
+<font size=5 color=red>红旗学院 独家：外院弟子—内院弟子—红旗战队 技术成长路线：</font>
+<font size=3>吸纳所有对信息安全抱有极大兴趣和学习热情的青年，成为红旗学院“外院弟子”在红旗学院内学习4个月，然后进入联盟各个企业的信息安全岗位进行实战历练；根据“外院弟子”各自的成长，不定期选拔、甄选成为红旗学院的“内院弟子”给予其莫大的机缘，传承“红旗学院”先锋技术；从“内院弟子”中挑选顶尖学员组成专业“红旗战队”有选择的参加国内外各种竞赛，使其在信息安全领域扬名立万！</font>
+
+<br>
+
+> <font size=3 color=red>中国（中关村）网络安全与信息化产业联盟</font>
+> -
+> <font size=3>项目实施：北京学邦世纪科技有限公司</font>
+> -
+> <font size=3>北京石景山区石景山路40号</font>
+> -
+> <font size=3>信安大厦六层A-C</font>
+> -
+> <font size=3>WWW . 红旗学院 . COM</font>
+> -
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,57 +1,62 @@
 
-var isSwiper = true;
-var saveSwiper = 0;
-var navItem = $('.nav-item');
-var navSelect = $('.select').eq(0);
-var swiper = new Swiper('.swiper-container', {
-    pagination: '.swiper-pagination',
-    direction: 'vertical',
-    speed: 1600,
-    onSlideChangeEnd: function(swiper){
-    	isSwiper = true;
-      	saveSwiper = swiper.activeIndex;
-      	navSelect.animate({"top": saveSwiper * 32 + 'px'}, 0);
-      	navItem.eq(saveSwiper).addClass('current').siblings().removeClass('current');
-    }
-});
+$(document).ready(function () {
+	var isSwiper = true;
+	var saveSwiper = 0;
+	var navItem = $('.nav-item');
+	var navSelect = $('.select').eq(0);
 
-$(document).on('mousewheel DOMMouseScroll', function (e) {
-	if (!isSwiper) {return false;};
-	isSwiper = false;
-    var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) || (e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));
-    if (delta > 0) {
-    	if (saveSwiper == 0) {isSwiper = true;};
-    	swiper.slidePrev();
-    }else if (delta < 0) {
-    	if (saveSwiper == navItem.length - 1) {isSwiper = true;};
-    	swiper.slideNext();
-    };
-});
+	var swiper = new Swiper('.swiper-container', {
+	    pagination: '.swiper-pagination',
+	    direction: 'vertical',
+	    speed: 1600,
+	    onSlideChangeEnd: function(swiper){
+	    	isSwiper = true;
+	      	saveSwiper = swiper.activeIndex;
+	      	navSelect.animate({"top": saveSwiper * 32 + 'px'}, 0);
+	      	navItem.eq(saveSwiper).addClass('current').siblings().removeClass('current');
+	    }
+	});
+	// swiper.slideTo(3, 0, true);
 
-navItem.on('click', function (event) {
-	event.preventDefault();
-	if (!isSwiper) {return false;}else {
+	$(document).on('mousewheel DOMMouseScroll', function (e) {
+		if (!isSwiper) {return false;};
 		isSwiper = false;
-		navItem.removeClass('current');
-		var i = $(this).index();
-		var _this = $(this);
-		navSelect.animate({"top": i * 32 + 'px'}, 600, function () {
-			_this.addClass('current').siblings().removeClass('current');
-		});
-		swiper.slideTo(i, 1800, true);
-	};
-});
+	    var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) || (e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));
+	    if (delta > 0) {
+	    	if (saveSwiper == 0) {isSwiper = true;};
+	    	swiper.slidePrev();
+	    }else if (delta < 0) {
+	    	if (saveSwiper == navItem.length - 1) {isSwiper = true;};
+	    	swiper.slideNext();
+	    };
+	});
 
-navItem.on('dblclick', function (event) {
-	event.preventDefault();
-	isSwiper = true;
-});
+	navItem.on('click', function (event) {
+		event.preventDefault();
+		if (!isSwiper) {return false;}else {
+			isSwiper = false;
+			navItem.removeClass('current');
+			var i = $(this).index();
+			var _this = $(this);
+			navSelect.animate({"top": i * 32 + 'px'}, 600, function () {
+				_this.addClass('current').siblings().removeClass('current');
+			});
+			swiper.slideTo(i, 1800, true);
+		};
+	});
 
-// $('.swiper-slide').eq(0).on('mousewheel DOMMouseScroll', function (event) {
-// 	var event = event || window.event;
-// 	event.stopPropagation();
-// 	return false;
-// });
+	navItem.on('dblclick', function (event) {
+		event.preventDefault();
+		isSwiper = true;
+	});
+
+	// $('.swiper-slide').eq(0).on('mousewheel DOMMouseScroll', function (event) {
+	// 	var event = event || window.event;
+	// 	event.stopPropagation();
+	// 	return false;
+	// });
+});
+	
 
 function loadJScript() {
 	var script = document.createElement("script");
